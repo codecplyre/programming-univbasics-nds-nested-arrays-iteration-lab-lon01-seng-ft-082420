b4807778b470b39d9fae89daf162f9f23db9ddb9
Ricky Adriell Cultura <pansoy.rickyadriell26457@gmail.com>
2020-03-10 23:05:43 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,5 +1,4 @@
 array_2 = [[-1, -900], [10, 30], [0, 0], [14, 16 * -2.5], [Math.sin(1), 19]]
-array_3 = [[86262, 58115], [22558, 97709], [66236, 28056], [25992, 81375], [11248, 56789], [42783, 27353], [70796, 63432], [53234, 63058], [72316, 8675309]]
 def join_ingredients(src)
   row_index = 0
   array = []
@@ -42,13 +41,10 @@ def total_even_pairs(src)
   while src.length > row_index do 
     column_index = 0 
     while column_index < src[row_index].length do 
-      if src[row_index][column_index] % 2 == 0 
-        total += src[row_index][column_index]
-      end
+      total += src[row_index][column_index]
       column_index += 1
     end
     row_index += 1 
   end
   total 
 end
-puts total_even_pairs(array_3)
