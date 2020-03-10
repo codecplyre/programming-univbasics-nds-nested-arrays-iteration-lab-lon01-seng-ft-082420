c7583d090d2028649b9e791361b6c668f7b9be69
Ricky Adriell Cultura <pansoy.rickyadriell26457@gmail.com>
2020-03-10 22:45:38 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -25,12 +25,8 @@ def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
-  row_index = 0
-  total = 0 
-  while row_index < src.length do 
-    column_index = 0
-    while column_index < src[row_index].length do 
-      
+
+    
 end
 
 def total_even_pairs(src)
